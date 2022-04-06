1dfdafe3a1ae66463c19a18953ed26cf076981f7
GerasimIvanov10 <gero.ivanov@gmail.com>
2022-04-06 00:16:42 -0700

    functions
    dec functions that convert the units

--- unit_converter.py
+++ unit_converter.py
@@ -1,6 +1,23 @@
-from tkinter import *
-import tkinter as tk
-from tkinter.ttk import Progressbar
-from time import sleep
-import webbrowser
+def cm_to_mm(cm):
+    return cm * 10
+print(cm_to_mm(float(input())))
 
+def mm_to_cm(mm):
+    return mm / 10
+print(mm_to_cm(float(input())))
+
+def m_to_cm(m):
+    return m * 100
+print(m_to_cm(float(input())))
+
+def cm_to_m(cm):
+    return cm / 100
+print(cm_to_m(float(input())))
+
+def km_to_m(km):
+    return km * 1000
+print(km_to_m(float(input())))
+
+def m_to_km(m):
+    return m / 1000
+print(m_to_km(float(input())))
