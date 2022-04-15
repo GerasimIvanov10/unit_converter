aaa9c1b36877ec77476991bf5d71e0f1c27efb72
GerasimIvanov10 <gero.ivanov@gmail.com>
2022-04-14 22:28:29 -0700

    Update unit_converter.py

--- unit_converter.py
+++ unit_converter.py
@@ -1,3 +1,10 @@
-units = {
-
+convert = {"km_to_m": lambda x: x * 1000,
+           "m_to_km": lambda x: x / 1000,
+           "cm_to_m": lambda x: x / 100,
+           "CtoF": lambda x: x * 1.8 + 32
 }
+
+some_distance_in_key = float(input())
+converted_distance = convert["CtoF"](some_distance_in_key)
+
+print(converted_distance)
